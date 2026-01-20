5316c4c8219acc623b3a03f875723ebfad7fb70f
Mark <mark.davidovich@gmail.com>
2026-01-21 01:13:16 +0200

    feat: Add new contact, prevent duplicate contact additions, and refactor contact deletion and UI module imports.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ui = require("./contactUi");
+const { printMessage, actionsMenu } = require("./contactUi");
 const { loadContacts, saveContacts, addContactToList, deleteContactFromList, searchContactInList } = require("./contactService");
 
 const FILE = "contacts.json";
@@ -17,15 +17,23 @@ const addContact = (name, email, phone) => {
 
     contactList = contacts;
     printMessage(`Loaded ${contactList.length} contacts`);
-
-    addContactToList(contacts, { name, email, phone });
-    printMessage(`Contact added: ${name}`);
-    saveContacts(FILE, contacts, callback);
-    printMessage(`Contacts saved to ${FILE}`);
+    if (!contactList.some((contact) => contact.email == email)) {
+      addContactToList(contacts, { name, email, phone });
+      printMessage(`Contact added: ${name}`);
+      saveContacts(FILE, contacts, (err) => {
+        if (err) {
+          printMessage(err.message);
+          return;
+        }
+      });
+      printMessage(`Contacts saved to ${FILE}`);
+    } else {
+      printMessage(`Contact already exists in ${FILE}`);
+    }
   });
 };
 
-const deleteContact = (contacts, email) => {
+const deleteContact = (email) => {
   printMessage(`Loading contacts from ${FILE}`);
   loadContacts(FILE, (err, contacts) => {
     if (err) {
@@ -37,31 +45,34 @@ const deleteContact = (contacts, email) => {
     printMessage(`Loaded ${contactList.length} contacts`);
 
     // deleteContactFromList(contacts, email);
-    let postDeleteInfo = deleteContactFromList(contacts, email);
+    let postDeleteInfo = deleteContactFromList(contactList, email);
     contactList = postDeleteInfo.newArray;
     printMessage(`Contact deleted ${postDeleteInfo.deletedName}`);
-    saveContacts(FILE, contacts, callback);
+    saveContacts(FILE, contactList, (err) => {
+      if (err) {
+        printMessage(err.message);
+        return;
+      }
+    });
     printMessage(`Contacts saved to ${FILE}`);
   });
-
-  const run = () => {
-    const input = ui.actionsMenu();
-    console.log("reached inside run function, input:", input);
-
-    switch (input[0]) {
-      case "add":
-        console.log("reached add");
-        addContact(input[1], input[2], input[3]);
-        break;
-      case "delete":
-        deleteContact(input[1]);
-        break;
-
-      default:
-        printMessage(`Error, unknown command '${input[0]}'`);
-        break;
-    }
-  };
-
-  run();
 };
+
+const run = () => {
+  const input = actionsMenu();
+
+  switch (input[0]) {
+    case "add":
+      addContact(input[1], input[2], input[3]);
+      break;
+    case "delete":
+      deleteContact(input[1]);
+      break;
+
+    default:
+      printMessage(`Error, unknown command '${input[0]}'`);
+      break;
+  }
+};
+
+run();
