6e48dbd96caadf3b567ffce1b6ce2583cb97c428
sashkasirotin <sashkasirotin@gmail.com>
2026-01-20 22:18:28 +0200

    updating contactsUi

--- contactUi.js
+++ contactUi.js
@@ -4,26 +4,41 @@ const contactService = require('./contactService')
 
 
 
+function help() {
+    console.log('Usage: node contacts.js [command] [arguments]\n' +
+        '\n\nCommands:\n' +
+        '  add "name" "email" "phone"  - Add a new contact\n' +
+        '  list                        - List all contacts\n' +
+        '  search "query"              - Search contacts by name or email\n' +
+        '  delete "email"              - Delete contact by email\n' +
+        '  help                        - Show this help message\n'
+
+
+
+
+
+
+    )
+}
 
 function actionsMenu() {
     console.log("arguments:", process.argv)
-    const action = process.argv[2]
+    let action = process.argv[2]
 
-    while (action !== 'exit') {
-        action = process.argv[2]
+    while (action != 'exit') {
+        //action = process.argv[2]
         // let name = process.argv[3]
         //let email = process.argv[4]
         //let phoneNumber = process.argv[5]
         console.log("action " + action)
-
+        action = process.argv[2]
         switch (action.toLowerCase()) {
             case ("add"):
                 //let name = process.argv[3]
                 //let email = process.argv[4]
                 //let phoneNumber = process.argv[5]
-
                 const contact = { name: process.argv[3], email: process.argv[4], phoneNumber: process.argv[5] }
-                console.log("email: " + email + "" + phoneNumber)
+                console.log("name:" + contact.name + "email: " + contact.email + "" + contact.phoneNumber)
                 contactService.addContact(email, phoneNumber)
                 break;
 
@@ -41,17 +56,18 @@ function actionsMenu() {
                 break;
             }
             case ("help"):
-                console.log('=== All Contacts ===')
-                break;
-            default: {
+                help()
                 action = 'exit'
                 break;
-            }
+            default:
+                action = 'exit'
+                break;
+
         }
     }
 }
 
-//actionsMenu()
+actionsMenu()
 
 module.exports = {
     actionsMenu
